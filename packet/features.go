c516a7ce3ce6c83e5e6548ba33656822aa499743
Daniel Ferreira <dcferreira@users.noreply.github.com>
2017-07-12 10:56:27 +0200

    added character features

--- packet/features.go
+++ packet/features.go
@@ -1,6 +1,8 @@
 package packet
 
 import (
+    "strings"
+    "bytes"
 	"net"
 
 	"encoding/binary"
@@ -760,4 +762,134 @@ func init() {
 		{flows.FeatureTypePacket, func() flows.Feature { return &_tcpNsTotalCountPacket{} }, nil},
 		{flows.FeatureTypeFlow, func() flows.Feature { return &_tcpNsTotalCountFlow{} }, nil},
 	})
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+type _characters struct {
+	flows.BaseFeature
+    time int64
+    src []byte
+}
+
+func (f *_characters) Event(new interface{}, when flows.Time, src interface{}) {
+    var time int64
+    if f.time != 0 {
+        time = int64(when) - f.time
+    }
+    if len(f.src) == 0 {
+        tmp_src, _ := new.(*packetBuffer).NetworkLayer().NetworkFlow().Endpoints()
+        f.src = tmp_src.Raw()
+    }
+    f.time = int64(when)
+    new_time := int(float64(time) / 100000000.)  // time is now in deciseconds
+
+    srcIP, _ := new.(*packetBuffer).NetworkLayer().NetworkFlow().Endpoints()
+
+    var buffer bytes.Buffer
+    if bytes.Equal(f.src, srcIP.Raw()) {
+        buffer.WriteString("A")
+    } else {
+        buffer.WriteString("B")
+    }
+
+    buffer.WriteString(strings.Repeat("_", new_time))
+
+    f.SetValue(buffer.String(), when, f)
+}
+
+func init() {
+    flows.RegisterFeature("_characters", []flows.FeatureCreator{
+        {flows.FeatureTypePacket, func() flows.Feature { return &_characters{} }, nil},
+    })
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+type _characters2 struct {
+	flows.BaseFeature
+    time int64
+    src []byte
+}
+
+func (f *_characters2) Event(new interface{}, when flows.Time, src interface{}) {
+    var time int64
+    if f.time != 0 {
+        time = int64(when) - f.time
+    }
+    if len(f.src) == 0 {
+        tmp_src, _ := new.(*packetBuffer).NetworkLayer().NetworkFlow().Endpoints()
+        f.src = tmp_src.Raw()
+    }
+    f.time = int64(when)
+    new_time := int(float64(time) / 100000000.)  // time is now in deciseconds
+
+    srcIP, _ := new.(*packetBuffer).NetworkLayer().NetworkFlow().Endpoints()
+    tcp := getTcp(new.(*packetBuffer))
+    if tcp == nil {
+        return
+    }
+
+    var buffer bytes.Buffer
+    if bytes.Equal(f.src, srcIP.Raw()) {
+        // A->B
+        if tcp.FIN && tcp.ACK {
+            buffer.WriteString("n")
+        } else if tcp.FIN {
+            buffer.WriteString("f")
+        } else if tcp.SYN && tcp.ACK {
+            buffer.WriteString("k")
+        } else if tcp.SYN{
+            buffer.WriteString("s")
+        } else if tcp.RST && tcp.ACK {
+            buffer.WriteString("t")
+        } else if tcp.RST {
+            buffer.WriteString("r")
+        } else if tcp.PSH && tcp.ACK {
+            buffer.WriteString("h")
+        } else if tcp.PSH {
+            buffer.WriteString("p")
+        } else if tcp.ACK {
+            buffer.WriteString("a")
+        } else if tcp.URG {
+            buffer.WriteString("u")
+        } else {
+            buffer.WriteString("o")
+        }
+    } else {
+        // B->A
+        if tcp.FIN && tcp.ACK {
+            buffer.WriteString("N")
+        } else if tcp.FIN {
+            buffer.WriteString("F")
+        } else if tcp.SYN && tcp.ACK {
+            buffer.WriteString("K")
+        } else if tcp.SYN{
+            buffer.WriteString("S")
+        } else if tcp.RST && tcp.ACK {
+            buffer.WriteString("T")
+        } else if tcp.RST {
+            buffer.WriteString("R")
+        } else if tcp.PSH && tcp.ACK {
+            buffer.WriteString("H")
+        } else if tcp.PSH {
+            buffer.WriteString("P")
+        } else if tcp.ACK {
+            buffer.WriteString("A")
+        } else if tcp.URG {
+            buffer.WriteString("U")
+        } else {
+            buffer.WriteString("O")
+        }
+    }
+
+    buffer.WriteString(strings.Repeat("-", new_time))
+
+    f.SetValue(buffer.String(), when, f)
+}
+
+func init() {
+    flows.RegisterFeature("_characters2", []flows.FeatureCreator{
+        {flows.FeatureTypePacket, func() flows.Feature { return &_characters2{} }, nil},
+    })
 }
