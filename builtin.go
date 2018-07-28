4b45dc1b19b66ca46172ecb056d11673ddbb4107
Gernot Vormayr <gernot.vormayr@nt.tuwien.ac.at>
2018-07-28 17:57:48 +0200

    Add time filter

--- builtin.go
+++ builtin.go
@@ -4,6 +4,7 @@ import (
 	_ "github.com/CN-TU/go-flows/modules/exporters/csv"
 	_ "github.com/CN-TU/go-flows/modules/exporters/ipfix"
 	_ "github.com/CN-TU/go-flows/modules/exporters/kafka"
+	_ "github.com/CN-TU/go-flows/modules/filters/time"
 	_ "github.com/CN-TU/go-flows/modules/labels/csv"
 	_ "github.com/CN-TU/go-flows/modules/sources/libpcap"
 )
---
+++ modules/filters/time/time.go
@@ -0,0 +1,134 @@
+package time
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/CN-TU/go-flows/packet"
+	"github.com/CN-TU/go-flows/util"
+	"github.com/google/gopacket"
+)
+
+type timeFilter struct {
+	id                      string
+	before, after           time.Time
+	checkbefore, checkafter bool
+}
+
+func (tf *timeFilter) ID() string {
+	return tf.id
+}
+
+func (tf *timeFilter) Init() {
+}
+
+func (tf *timeFilter) Matches(ci gopacket.CaptureInfo, data []byte) bool {
+	if tf.checkbefore && tf.before.Before(ci.Timestamp) {
+		return true
+	}
+	if tf.checkafter && ci.Timestamp.Before(tf.after) {
+		return true
+	}
+	return false
+}
+
+func newTimeFilter(name string, opts interface{}, args []string) (arguments []string, ret util.Module, err error) {
+	var before, after time.Time
+	var checkbefore, checkafter bool
+
+	if _, ok := opts.(util.UseStringOption); ok {
+		if len(args) == 0 {
+			return nil, nil, errors.New("time filter needs a keyword (before, after, between) and time, was given none")
+		}
+		switch args[0] {
+		case "before":
+			if len(args) < 2 {
+				return nil, nil, errors.New("time filter keyword 'before' needs a time argument")
+			}
+			checkbefore = true
+			before, err = time.Parse(time.RFC3339Nano, args[1])
+			if err != nil {
+				return
+			}
+			arguments = args[2:]
+		case "after":
+			if len(args) < 2 {
+				return nil, nil, errors.New("time filter keyword 'after' needs a time argument")
+			}
+			checkafter = true
+			after, err = time.Parse(time.RFC3339Nano, args[1])
+			if err != nil {
+				return
+			}
+			arguments = args[2:]
+		case "between":
+			if len(args) < 3 {
+				return nil, nil, errors.New("time filter keyword 'between' needs two time arguments")
+			}
+			checkafter = true
+			after, err = time.Parse(time.RFC3339Nano, args[1])
+			if err != nil {
+				return
+			}
+			checkbefore = true
+			before, err = time.Parse(time.RFC3339Nano, args[2])
+			if err != nil {
+				return
+			}
+			arguments = args[3:]
+		default:
+			return nil, nil, fmt.Errorf("time filter needs a keyword (before, after, between), but was given '%s'", args[0])
+		}
+	} else {
+		panic("FIXME: implement this")
+	}
+
+	if name == "" {
+		name = "time"
+		if checkafter {
+			name += fmt.Sprint("|>", after)
+		}
+		if checkbefore {
+			name += fmt.Sprint("|>", before)
+		}
+	}
+
+	ret = &timeFilter{
+		id:          name,
+		before:      before,
+		after:       after,
+		checkafter:  checkafter,
+		checkbefore: checkbefore,
+	}
+	return
+}
+
+func timeHelp(name string) {
+	fmt.Fprintf(os.Stderr, `
+The %s filter allows to filter packets based on time. Times must be
+specified according to RFC3339 and can be given in  nanosecond accuracy.
+
+Possible filters:
+  before <time>
+    Only packets with time < <time> are accepted.
+  after <time>
+    Only packets with time > <time> are accepted.
+  between <a> <b>
+    Only packets with <a> < arrival < <b> are accepted.
+
+Usage command line:
+  filter %s before|after|bewteen time [time]
+
+Usage json file (not working):
+  {
+    "type": "%s",
+    "options": "file.csv"
+  }
+`, name, name, name)
+}
+
+func init() {
+	packet.RegisterFilter("time", "Filter packets based on time.", newTimeFilter, timeHelp)
+}
