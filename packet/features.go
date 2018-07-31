71ff5d4feb3c049f76de342e5ebc3cc77e74b01e
Gernot Vormayr <gernot.vormayr@nt.tuwien.ac.at>
2018-07-31 17:17:20 +0200

    DateTime is a point in time - not a difference

--- packet/features.go
+++ packet/features.go
@@ -367,19 +367,19 @@ func (f *_interPacketTimeNanoseconds) Start(context *flows.EventContext) {
 }
 
 func (f *_interPacketTimeNanoseconds) Event(new interface{}, context *flows.EventContext, src interface{}) {
-	var time flows.DateTimeNanoseconds
+	var time int64
 	if f.time != 0 {
-		time = context.When() - f.time
+		time = int64(context.When()) - int64(f.time)
 	}
 	f.time = context.When()
 	f.SetValue(time, context, f)
 }
 
 func init() {
-	flows.RegisterTemporaryFeature("_interPacketTimeNanoseconds", ipfix.DateTimeNanosecondsType, 0, flows.PacketFeature, func() flows.Feature { return &_interPacketTimeNanoseconds{} }, flows.RawPacket)
-	flows.RegisterTemporaryFeature("_interPacketTimeMicroseconds", ipfix.DateTimeMicrosecondsType, 0, flows.PacketFeature, func() flows.Feature { return &_interPacketTimeNanoseconds{} }, flows.RawPacket)
-	flows.RegisterTemporaryFeature("_interPacketTimeMilliseconds", ipfix.DateTimeMillisecondsType, 0, flows.PacketFeature, func() flows.Feature { return &_interPacketTimeNanoseconds{} }, flows.RawPacket)
-	flows.RegisterTemporaryFeature("_interPacketTimeSeconds", ipfix.DateTimeSecondsType, 0, flows.PacketFeature, func() flows.Feature { return &_interPacketTimeNanoseconds{} }, flows.RawPacket)
+	flows.RegisterTemporaryFeature("_interPacketTimeNanoseconds", ipfix.Signed64Type, 0, flows.PacketFeature, func() flows.Feature { return &_interPacketTimeNanoseconds{} }, flows.RawPacket)
+	flows.RegisterTemporaryCompositeFeature("_interPacketTimeMicroseconds", ipfix.Signed64Type, 0, "divide", "_interPacketTimeNanoseconds", 1000)
+	flows.RegisterTemporaryCompositeFeature("_interPacketTimeMilliseconds", ipfix.Signed64Type, 0, "divide", "_interPacketTimeNanoseconds", 1000000)
+	flows.RegisterTemporaryCompositeFeature("_interPacketTimeSeconds", ipfix.Signed32Type, 0, "divide", "_interPacketTimeNanoseconds", 1000000000)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
