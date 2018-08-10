8ad06f80d45b315d9312d1e31fe8b5d2d22fe92a
Gernot Vormayr <gernot.vormayr@nt.tuwien.ac.at>
2018-08-10 20:02:16 +0200

    Document testing library

--- packet_test/testing.go
+++ packet_test/testing.go
@@ -10,13 +10,19 @@ import (
 	"github.com/CN-TU/go-flows/packet"
 )
 
+// FeatureResult holds the result for the named feature
 type FeatureResult struct {
-	Name  string
+	// Name of the feature
+	Name string
+	// Value of the feature
 	Value interface{}
 }
 
+// FeatureLine holds one record (i.e. Feature results + point in time)
 type FeatureLine struct {
-	When     flows.DateTimeNanoseconds
+	// When is the point in time this was exported
+	When flows.DateTimeNanoseconds
+	// Features is a list of feature results
 	Features []FeatureResult
 }
 
@@ -51,12 +57,14 @@ func (ae *assertExporter) ID() string {
 func (ae *assertExporter) Init() {
 }
 
+// TestTable is flow table implementation which can be used for testing
 type TestTable struct {
 	exporter *assertExporter
 	table    *flows.FlowTable
 	t        *testing.T
 }
 
+// MakeFeatureTest creates a flow table for testing purposes with the given features, wanted return type, and flow options
 func MakeFeatureTest(t *testing.T, features []string, ft flows.FeatureType, opt flows.FlowOptions) (ret TestTable) {
 	ret.t = t
 	if opt.ActiveTimeout == 0 {
@@ -76,14 +84,17 @@ func MakeFeatureTest(t *testing.T, features []string, ft flows.FeatureType, opt 
 	return
 }
 
+// MakeFlowFeatureTest returns a flow table for testing a single flow feature
 func MakeFlowFeatureTest(t *testing.T, feature string) TestTable {
 	return MakeFeatureTest(t, []string{feature}, flows.FlowFeature, flows.FlowOptions{})
 }
 
+// MakePacketFeatureTest returns a flow table for testing a single packet feature
 func MakePacketFeatureTest(t *testing.T, feature string) TestTable {
 	return MakeFeatureTest(t, []string{feature}, flows.PacketFeature, flows.FlowOptions{ActiveTimeout: 0})
 }
 
+// EventLayers simulates a packet arriving at the given point in time with the given layers populated
 func (t *TestTable) EventLayers(when flows.DateTimeNanoseconds, layerList ...packet.SerializableLayerType) {
 	data := packet.BufferFromLayers(when, layerList...)
 	key, fw := packet.Fivetuple(data, false)
@@ -91,6 +102,7 @@ func (t *TestTable) EventLayers(when flows.DateTimeNanoseconds, layerList ...pac
 	t.table.Event(data)
 }
 
+// Finish finalizes all the flows in the table
 func (t *TestTable) Finish(when flows.DateTimeNanoseconds) {
 	t.table.EOF(when)
 	t.exporter.Finish()
@@ -123,10 +135,11 @@ func (t *TestTable) sprintObserved() string {
 	return strings.Join(lines, "\n")
 }
 
-func (t *TestTable) Fail(msg string) {
+func (t *TestTable) fail(msg string) {
 	t.t.Errorf("%s\nObserved Features:\n%s\n", msg, t.sprintObserved())
 }
 
+// AssertFeatureValue fails the test if the named feature did not return the expected value
 func (t *TestTable) AssertFeatureValue(name string, value interface{}) {
 	for _, line := range t.exporter.seen {
 		for _, feature := range line.Features {
@@ -135,28 +148,29 @@ func (t *TestTable) AssertFeatureValue(name string, value interface{}) {
 			}
 		}
 	}
-	t.Fail(fmt.Sprintf("Couldn't observe %s == %v", name, value))
+	t.fail(fmt.Sprintf("Couldn't observe %s == %v", name, value))
 }
 
+// AssertFeatureList fails the test if the table did not export the given resultset
 func (t *TestTable) AssertFeatureList(result []FeatureLine) {
 	for i, line := range result {
 		if i >= len(t.exporter.seen) {
-			t.Fail(fmt.Sprintf("Expected another result line - but only %d seen", i))
+			t.fail(fmt.Sprintf("Expected another result line - but only %d seen", i))
 			return
 		}
 		realLine := t.exporter.seen[i]
 		if line.When != realLine.When {
-			t.Fail(fmt.Sprintf("Result time #%d mismatches (is %s, expected %s)", i+1, pprintTime(line.When), pprintTime(realLine.When)))
+			t.fail(fmt.Sprintf("Result time #%d mismatches (is %s, expected %s)", i+1, pprintTime(line.When), pprintTime(realLine.When)))
 			return
 		}
 		for j, feature := range line.Features {
 			if j >= len(realLine.Features) {
-				t.Fail(fmt.Sprintf("Expected %d features @%s, but only %d seen", len(line.Features), pprintTime(line.When), len(realLine.Features)))
+				t.fail(fmt.Sprintf("Expected %d features @%s, but only %d seen", len(line.Features), pprintTime(line.When), len(realLine.Features)))
 				return
 			}
 			realFeature := realLine.Features[j]
 			if feature.Name != realFeature.Name || !reflect.DeepEqual(feature.Value, realFeature.Value) {
-				t.Fail(fmt.Sprintf("Result @%s mismatches; wanted %s = %v, but got %s = %v", pprintTime(line.When), feature.Name, feature.Value, realFeature.Name, realFeature.Value))
+				t.fail(fmt.Sprintf("Result @%s mismatches; wanted %s = %v, but got %s = %v", pprintTime(line.When), feature.Name, feature.Value, realFeature.Name, realFeature.Value))
 				return
 			}
 		}
