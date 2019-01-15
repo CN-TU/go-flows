3dc40d28a3b2c7e6a6f6a64244a346f78d884606
Gernot Vormayr <gernot.vormayr@nt.tuwien.ac.at>
2019-01-15 14:40:59 +0100

    Enable nta features

--- builtin.go
+++ builtin.go
@@ -6,10 +6,12 @@ import (
 	_ "github.com/CN-TU/go-flows/modules/exporters/kafka"
 	_ "github.com/CN-TU/go-flows/modules/features/custom"
 	_ "github.com/CN-TU/go-flows/modules/features/iana"
+	_ "github.com/CN-TU/go-flows/modules/features/nta"
 	_ "github.com/CN-TU/go-flows/modules/features/operations"
 	_ "github.com/CN-TU/go-flows/modules/features/staging"
 	_ "github.com/CN-TU/go-flows/modules/filters/time"
 	_ "github.com/CN-TU/go-flows/modules/keys/header"
+	_ "github.com/CN-TU/go-flows/modules/keys/time"
 	_ "github.com/CN-TU/go-flows/modules/labels/csv"
 	_ "github.com/CN-TU/go-flows/modules/sources/libpcap"
 )
